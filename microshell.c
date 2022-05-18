83632b58ccda49e14898af868cf114d1173a0e22
Fredi-B <fredibechtoldt@outlook.com>
2022-05-18 19:44:03 +0200

    going for shackis approach. work in progress

--- microshell.c
+++ microshell.c
@@ -1,67 +1,68 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 
+void	puterr(char *msg, char *arg)
+{
+	while (*msg)
+		write(2, msg++, 1);
+	if (arg)
+	{
+		while (*arg)
+			write(2, arg++, 1);
+	}
+	write(2, "\n", 1);
+}
+
+// warum returnt shacki? hier und oben
+void	ft_execute(char **argv, int i, int tmp_fd, char **env)
+{
+	argv[i] = NULL;
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
+	execve(argv[0], argv, env);
+	puterr("error: cannot execute ", argv[0]);
+}
+
 int	main(int argc, char **argv, char **env)
 {
-	int	pipe_flag_write;
-	int	pipe_flag_read;
-	int	begin_of_arg_list;
-	int	id;
-	int	fd[2];
-	int	temp_fd;
+	(void)argc;
 	int	i;
+	int	fd[2];
+	int	tmp_fd;
+	int	id;
 
-	i = 1;
-	begin_of_arg_list = i;
-	while (argv[i])
+	i = 0;
+	tmp_fd = dup(STDIN_FILENO);
+	while (argv[i] && argv[i + 1])
 	{
-		pipe_flag_write = 0;
-		if (strncmp(argv[i], "|\0", 3) == 0 || strncmp(argv[i], ";\0", 3) == 0 )
+		argv = &argv[i + 1];
+		i = 0;
+		while (argv[i] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
+			i++;
+		if (strcmp(argv[0], "cd") == 0)
 		{
-			if (strncmp(argv[i], "|\0", 3) == 0)
-			{
-				pipe(fd);
-				pipe_flag_write = 1;
-			}
-			argv[i] = NULL;
+			if (i != 2)
+				puterr("error: cd: bad arguments", NULL);
+			else if (chdir(argv[1]))
+				puterr("error: cd: cannot change directory to ", argv[1]);
+		}
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		{
 			id = fork();
 			if (id == 0)
+				ft_execute(argv, i, tmp_fd, env);
+			else
 			{
-				if (pipe_flag_write == 1)
-				{
-					close(fd[0]);
-					dup2(fd[1], STDOUT_FILENO);
-					close(fd[1]);
-				}
-				execve(argv[begin_of_arg_list], &argv[begin_of_arg_list], env);
+				close(tmp_fd);
+				while (waitpid(-1, NULL, WUNTRACED))
+					;
+				tmp_fd = dup(STDIN_FILENO);
 			}
-			if (pipe_flag_write == 1)
-			{
-				pipe_flag_write = 0;
-				close(fd[1]);
-				temp_fd = fd[0];
-				pipe_flag_read = 1;
-			}
-			begin_of_arg_list = i + 1;
 		}
-		i++;
 	}
-	id = fork();
-	if (id == 0)
-	{
-		if (pipe_flag_read == 1)
-		{
-			dup2(temp_fd, STDIN_FILENO);
-			close(temp_fd);
-		}
-		execve(argv[begin_of_arg_list], &argv[begin_of_arg_list], env);
-	}
-	if (pipe_flag_read == 1)
-		close(temp_fd);
-	while (waitpid(-1, 0, 0) != -1)
-		;
 	return (0);
 }
 
